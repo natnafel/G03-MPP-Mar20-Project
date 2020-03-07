cbe8e922f6989253e3cf39f5a6f39b42ae421f24
Nischal Dhakal <nisch4ldh4k4l@gmail.com>
2020-03-07 00:37:35 -0600

    fixes #17 Creates all UI designs. Implements Login, Navigation, Logout features

--- src/ui/controllers/LoginController.java
+++ src/ui/controllers/LoginController.java
@@ -1,10 +1,18 @@
 package ui.controllers;
 
 import business.AccountService;
+import business.User;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class LoginController {
 
@@ -12,13 +20,32 @@ public class LoginController {
     private JFXTextField usernameField;
 
     @FXML
+    private Label loginErrorMsg;
+
+    @FXML
     private JFXPasswordField passwordField;
     private AccountService accountService = new AccountService();
 
 
-    public void login(Event event){
-        boolean isValid = accountService.validateUser(usernameField.getText(), passwordField.getText() );
-        System.out.println(isValid);
+    public void login(Event event) throws Exception {
+        User user = accountService.validateUser(usernameField.getText(), passwordField.getText() );
+        if(user == null){
+            loginErrorMsg.setVisible(true);
+        }
+        else{
+            User.setLoggedInUser(user);
+            loginErrorMsg.setVisible(false);
+            Parent root = FXMLLoader.load(getClass().getResource("../resources/Dashboard.fxml"));
+            Scene scene = new Scene(root, 901.0, 455.0);
+            Stage stage = new Stage();
+            stage.setTitle("Dashboard");
+            stage.setScene(scene);
+            stage.show();
+            Stage stage1 = (Stage) loginErrorMsg.getScene().getWindow();
+            stage1.close();
+        }
+
+
     }
 
 }
